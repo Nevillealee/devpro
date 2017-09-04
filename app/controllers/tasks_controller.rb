3e88f90b6a12b64190b9708124becec28f4b3799
nevillealee <nevillealee@gmail.com>
2017-09-04 05:08:30 +0000

    comments added to index action within tasks controller for clarity

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
     def index
-        render json: Task.all
+        render json: Task.all #instead of html send user JSON data (dont go to views just send json data from DB)
     end
 end
