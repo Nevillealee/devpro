9306abf52b00cf702bbcef739ed9c5287c545c8b
nevillealee <nevillealee@gmail.com>
2017-09-06 00:12:40 +0000

    using TDD we added ability to update task in db through our ReSTful json api

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
     def index
         render json: Task.all #instead of html send user JSON data (dont go to views just send json data from DB)
     end
+    
+    def update
+        task = Task.find(params[:id]) #finds value in objects id field from db and matches it with id value from endpoint
+        task.update_attributes(task_params)
+        render json: task #show JSON representation of what we just updated
+    end
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:done) #these parameters in permit(:param) are being saved when user hits submit because we speifiy that here by listing those fields we want included.
+    end
 end
