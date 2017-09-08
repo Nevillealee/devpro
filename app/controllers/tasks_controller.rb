648c9c0a350ba91424f6a4a9b168558769dfbdbf
nevillealee <nevillealee@gmail.com>
2017-09-08 08:11:55 +0000

    tasks ordered on index view by db :id as in a queue vs default stack

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     def index
-        render json: Task.all #instead of html send user JSON data (dont go to views just send json data from DB)
+        render json: Task.order(:id) #instead of html send user JSON data (dont go to views just send json data from DB)
     end
     
     def update
