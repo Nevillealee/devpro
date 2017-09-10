cc7823810cca504da4dd465abb9e63f4e0f07fc6
nevillealee <nevillealee@gmail.com>
2017-09-10 04:58:34 +0000

    task#create rspec test pass
    add create action to tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,11 @@ class TasksController < ApplicationController
         render json: Task.order(:id) #instead of html send user JSON data (dont go to views just send json data from DB)
     end
     
+    def create
+        task = Task.create(task_params)
+        render json: task  #return JSON data back to client following request
+    end
+    
     def update
         task = Task.find(params[:id]) #finds value in objects id field from db and matches it with id value from endpoint
         task.update_attributes(task_params)
@@ -12,6 +17,6 @@ class TasksController < ApplicationController
     private
     
     def task_params
-        params.require(:task).permit(:done) #these parameters in permit(:param) are being saved when user hits submit because we speifiy that here by listing those fields we want included.
+        params.require(:task).permit(:done, :title) #these parameters in permit(:param) are being saved when user hits submit because we speifiy that here by listing those fields we want included.
     end
 end
